69e0f161c4b521c746fccab09ee15c75dede601a
Rich Boakes <rjb@port.ac.uk>
2023-12-06 17:13:21 +0000

    cleaned up

--- index.js
+++ index.js
@@ -1,36 +1,16 @@
-const doorNums = [ 24, 1, 18, 3, 4, 5, 6, 
-    20, 21, 22, 23,
-    7, 8, 9, 10, 2, 11, 12, 13, 
-];
 
-const images = [
-    "https://placekitten.com/100/100",
-    "https://placekitten.com/110/100",
-    "https://placekitten.com/120/100",
-    "https://placekitten.com/130/100",
-    "https://placekitten.com/140/100",
-    "https://placekitten.com/150/100",
-    "https://placekitten.com/160/100",
-    "https://placekitten.com/170/100",
-    "https://placekitten.com/180/100",
-    "https://placekitten.com/190/100",
-    "https://placekitten.com/200/100",
-    "https://placekitten.com/210/100",
-    "https://placekitten.com/220/100",
-    "https://placekitten.com/230/100",
-    "https://placekitten.com/240/100",
-    "https://placekitten.com/200/100",
-    "https://placekitten.com/200/100",
-    "https://placekitten.com/200/100",
-    "https://placekitten.com/220/100",
-    "https://placekitten.com/230/100",
-    "https://placekitten.com/240/100",
-    "https://placekitten.com/200/100",
-    "https://placekitten.com/200/100",
-    "https://placekitten.com/200/100",
-]
+// create an array of 24 numbers & sort it randomly
+const doorNums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,18,20,21,22,23,24];
+doorNums.sort(() => Math.random() - 0.5);
 
-function createImage(num="x", src="https://placekitten.com/100/100", alt="MERRY XMAS!") {
+// NOTE: this is technically hotlinking because I'm using the images from someone elses
+// site, which is bad practice *unless* you run the other site (or you have permission).
+const baseURL = "https://soc.port.ac.uk/staff/pix/"
+
+const staff = ["alaa", "shikun", "simon", "elly", "nadim", "amanda", "farzad", "annie", "taiwo", "oluwatobi", "gail", "kirsten", "thanos", "penny", "mani", "val", "claudia", "dalin", "haythem", "janka", "matthew", "ronel", "matt", "rich"]
+
+
+function createImage(num="x", src="./", alt="MERRY XMAS!") {
 
     const fig = document.createElement('figure');
     const cap = document.createElement('figcaption');
@@ -45,21 +25,32 @@ function createImage(num="x", src="https://placekitten.com/100/100", alt="MERRY 
     fig.append(cap);
 
     return fig;
+}
 
+function toggle(e) {
+    e.currentTarget.classList.toggle('open');
 }
 
 const target = document.querySelector('main');
 
-let i=0;
+// get current day of the month 
+const today = new Date().getDate();
+
 for (const num of doorNums) {
-    target.append( createImage(num, images[i]) );
-    i++;
+    // NOTE: this is technically hotlinking because I'm using the images from someone elses
+    // site, which is bad practice *unless* you run the other site (or you have permission).
+    const name = staff[num-1];
+    const image = `${baseURL}${name}/${name}.avif`
+    const door = createImage(num, image, `A picture of ${name} from the School of Computing`)
+    target.append( door );
+
+    // add an event listener to the door if it's a day before today
+    if (num <= today) {
+        door.addEventListener('click', toggle)
+    } else {
+        door.classList.add('locked');
+    }
+
 }
 
 const doors = document.querySelectorAll('figure');
-
-for (const door of doors) {
-    door.addEventListener('click', function() {
-        door.classList.toggle('open');
-    })
-}
